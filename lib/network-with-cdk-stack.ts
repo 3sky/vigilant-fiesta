bffb62f7a3853735609a8b29b085bb68c9668199
Kuba Wolynko <kuba@3sky.dev>
2023-08-29 14:46:58 +0200

    add network

--- lib/network-with-cdk-stack.ts
+++ lib/network-with-cdk-stack.ts
@@ -1,16 +1,58 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class NetworkWithCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    cdk.Tags.of(this).add("description", "OKD - Testing");
+    cdk.Tags.of(this).add("organization", "3sky.dev");
+    cdk.Tags.of(this).add("owner", "kuba");
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'NetworkWithCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const myVPC = new ec2.Vpc(this, 'MyVPC', {
+      ipAddresses: ec2.IpAddresses.cidr('10.192.0.0/20'),
+      maxAzs: 2,
+      natGateways: 2,
+      subnetConfiguration: [
+        {
+          cidrMask: 26,
+          name: 'ingress',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 26,
+          name: 'application',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          cidrMask: 28,
+          name: 'rds',
+          subnetType: ec2.SubnetType.PRIVATE_ISOLATED,
+        },
+      ]
+    });
+
+    new ec2.Instance(this, 'targetInstance', {
+      vpc: myVPC,
+      instanceType: ec2.InstanceType.of(ec2.InstanceClass.BURSTABLE2, ec2.InstanceSize.MICRO),
+      machineImage: new ec2.AmazonLinuxImage({ generation: ec2.AmazonLinuxGeneration.AMAZON_LINUX_2 }),
+      instanceName: "TestingInstance",
+      allowAllOutbound: true,
+      detailedMonitoring: true,
+      role: new iam.Role(this, "TestingEc2Role", {
+        roleName: "TestingEc2Role",
+        assumedBy: new iam.ServicePrincipal("ec2.amazonaws.com"),
+        description: "Test CDK with intances",
+        path: '/',
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonSSMManagedInstanceCore"),
+        ],
+      }),
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+      },
+    });
   }
 }
--- test/network-with-cdk.test.ts
+++ test/network-with-cdk.test.ts
@@ -1,17 +1,99 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as NetworkWithCdk from '../lib/network-with-cdk-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Capture, Template } from 'aws-cdk-lib/assertions';
+import * as NetworkWithCdk from '../lib/network-with-cdk-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/network-with-cdk-stack.ts
-test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+test('VPC has correct range', () => {
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::EC2::VPC', {
+        CidrBlock: "10.192.0.0/20",
+        EnableDnsHostnames: true,
+        EnableDnsSupport: true
+        //     VisibilityTimeout: 300
+    });
+});
+
+test('VPC has 6 subnets', () => {
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::Subnet', 6);
+
+});
+
+test('VPC has 2 NATs', () => {
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::NatGateway', 2);
+});
+
+test('VPC has 1 Igtw', () => {
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::InternetGateway', 1);
+});
+
+test('Stack has one instance', () => {
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.resourceCountIs('AWS::EC2::Instance', 1);
+});
+
+test('Instance has correct properties', () => {
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::EC2::Instance', {
+        InstanceType: "t2.micro",
+        Monitoring: true,
+    });
+});
+
+test("Check security group settings", () => {
+
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.hasResourceProperties('AWS::EC2::SecurityGroup', {
+        SecurityGroupEgress: [
+            { "CidrIp": "0.0.0.0/0" }
+        ],
+    });
+});
+
+test("Check is InstanceProfile exists", () => {
+
+    const app = new cdk.App();
+    //     // WHEN
+    const stack = new NetworkWithCdk.NetworkWithCdkStack(app, 'MyTestStack');
+    //     // THEN
+    const template = Template.fromStack(stack);
+
+    template.hasResource('AWS::IAM::InstanceProfile', {});
 });
